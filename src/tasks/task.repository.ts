cb0ec3b6a62581fb7984c9ada879dffdf0ba3568
Mwangi Shadrack <shaddy.mwangi@gmail.com>
2021-10-29 06:43:09 +0300

    task service improvements using the repository pattern

--- src/tasks/task.repository.ts
+++ src/tasks/task.repository.ts
@@ -1,5 +1,20 @@
+import { TaskStatus } from './task-status.enum';
+import { CreateTaskDto } from './dto/create-task.dto';
 import { Task } from './task.entity';
 import { EntityRepository, Repository } from 'typeorm';
 
 @EntityRepository(Task)
-export class TaskRepository extends Repository<Task> {}
+export class TaskRepository extends Repository<Task> {
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const { title, description } = createTaskDto;
+
+    const task = this.create({
+      title,
+      description,
+      status: TaskStatus.COMPLETE,
+    });
+
+    await this.save(task);
+    return task;
+  }
+}
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,7 +1,6 @@
 import { CreateTaskDto } from './dto/create-task.dto';
 import { TaskRepository } from './task.repository';
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { TaskStatus } from './task-status.enum';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Task } from './task.entity';
 
@@ -20,15 +19,6 @@ export class TasksService {
   }
 
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
-    const { title, description } = createTaskDto;
-
-    const task = this.tasksRespository.create({
-      title,
-      description,
-      status: TaskStatus.COMPLETE,
-    });
-
-    await this.tasksRespository.save(task);
-    return task;
+    return this.tasksRespository.createTask(createTaskDto);
   }
 }
